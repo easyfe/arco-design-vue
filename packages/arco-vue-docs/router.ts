6a0fef2c48d19717f7c634635991383e398515e6
han3sui <han3sui@gmail.com>
2026-03-24 13:50:58 +0800

    fix: github pages 404修复

--- packages/arco-vue-docs/router.ts
+++ packages/arco-vue-docs/router.ts
@@ -789,7 +789,7 @@ routes.push({ path: '/:pathMatch(.*)*', redirect: '/vue/docs/start' });
 nProgress.configure({ minimum: 0.4, showSpinner: false });
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('/arco-design-vue/'),
   routes,
   scrollBehavior(to, from, savedPosition) {
     // Always scroll to the top
