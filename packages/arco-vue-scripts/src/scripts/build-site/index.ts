02683d2faed38fdde05a762c2774660e69344417
han3sui <han3sui@gmail.com>
2026-03-24 13:31:56 +0800

    fix: github pages 404修复

--- packages/arco-vue-scripts/src/scripts/build-site/index.ts
+++ packages/arco-vue-scripts/src/scripts/build-site/index.ts
@@ -1,8 +1,13 @@
 import { build } from 'vite';
+import { copyFileSync } from 'fs';
+import { resolve } from 'path';
 import config from '../../configs/vite.site.prod';
 
 async function run() {
   await build(config);
+
+  const outDir = resolve(process.cwd(), 'dist');
+  copyFileSync(resolve(outDir, 'index.html'), resolve(outDir, '404.html'));
 }
 
 export default run;
